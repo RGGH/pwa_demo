88822cf2d0c76ec9c357b43190a76ac667c524fd
RGGH <pisscompany@gmail.com>
2024-10-22 08:47:13 +0100

    initial commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'hello-world-pwa';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+// Install event: cache all the assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets');
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch event: serve cached assets if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
